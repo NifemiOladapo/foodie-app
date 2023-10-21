9d5672c972877760b41fce4dfd731a004d8666ce
Nifemi <nifethedev@gmail.com>
2023-10-21 22:18:30 +0100

    updated app

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,11 +4,15 @@ const main__nav = document.getElementById("main__nav");
 let smallPhoneNavIsOpen = false;
 
 window.addEventListener("resize", () => {
-  if (window.innerWidth > "800") {
+  if (window.innerWidth > 810) {
     main__nav.style.display = "flex";
+    smallPhoneNavIsOpen = false;
   }
-  if (window.innerWidth < "801") {
-    main__nav.style.display = "none";
+  if (window.innerWidth <= 810) {
+    if(!smallPhoneNavIsOpen){
+      main__nav.style.display = "none";
+
+    }
   }
 });
 
